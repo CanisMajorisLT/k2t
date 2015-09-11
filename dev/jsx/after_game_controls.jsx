5092e2bc739e48e45d368c581a34759bcd5a7fc4
Vytenis Butkevičius <vytenis.butkevicius@adform.com>
2015-09-11 17:44:44 +0300

    startes structuring app for training and play same text again

--- dev/jsx/after_game_controls.jsx
+++ dev/jsx/after_game_controls.jsx
@@ -2,12 +2,20 @@ let React = require('react');
 
 let AfterGameControls = React.createClass({
 
+  handlePlayThisTextAgain(){
+  	// nusiusti text ID i playareawrap/gamewrap
+  },
+
+  handleTrainMistakes(){
+  	// render main_mistakes_training_component somethere
+  },
+
   render() {
     return (
       <div>
-          <button className="after-game-btn">Play this text again</button>
+          <button onClick={this.handlePlayThisTextAgain} className="after-game-btn">Play this text again</button>
           <button onClick={this.props.startNewGame} className="after-game-btn">Play different text</button>
-          <button className="after-game-btn">Train words with mistakes</button>
+          <button onClick={this.handleTrainMistakes} className="after-game-btn">Train words with mistakes</button>
       </div>
     );
   }
